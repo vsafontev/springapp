b40208534e8d1a6805d1b06053e6729c32a8b4e6
vsafontev <vladislavsafontev@gmail.com>
2022-06-19 15:43:54 +0300

    commit6

--- src/main/java/ru/safontev/springcourse/MusicPlayer.java
+++ src/main/java/ru/safontev/springcourse/MusicPlayer.java
@@ -1,12 +1,42 @@
 package ru.safontev.springcourse;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+
+    public MusicPlayer(){}
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+
+
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: musicList){
+            System.out.println("Playing: " + music.getSong());
+        }
+
     }
 }
---
+++ src/main/java/ru/safontev/springcourse/RapMusic.java
@@ -0,0 +1,8 @@
+package ru.safontev.springcourse;
+
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Rap music";
+    }
+}
